3d37d0c273d0aa71d8bdf4c397f5364e41951971
attawitkedkaew <attawit.k@gmail.com>
2023-01-26 09:38:24 +0700

    vue hello world and component

---
+++ learn-coding/week-7,8/hello-world/src/App.vue
@@ -0,0 +1,36 @@
+<template>
+  <div>
+    <HelloWorld msg="Welcome to Your Vue.js App" />
+    <ocean-comp v-if="false"> </ocean-comp>
+    <v-for-comp v-if="true"> </v-for-comp>
+    <profile-comp></profile-comp>
+  </div>
+</template>
+
+<script>
+import HelloWorld from "./components/HelloWorld.vue";
+import OceanComp from "./components/OceanComp.vue";
+import VForComp from "./components/VForComp.vue";
+import ProfileComp from "./components/ProfileComp.vue";
+
+export default {
+  name: "App",
+  components: {
+    HelloWorld,
+    OceanComp,
+    VForComp,
+    ProfileComp,
+  },
+};
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ learn-coding/week-7,8/hello-world/src/components/VForComp.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="row">
+    <div class="mx-auto col-12">
+      <h3>V-For Examples</h3>
+    </div>
+    <div
+      class="col-lg-3 col-md-4 col-6 p-1"
+      v-for="(item, index) in datas"
+      :key="index"
+    >
+      <img :src="item" width="200" />
+    </div>
+  </div>
+</template>
+<script>
+export default {
+  data() {
+    return {
+      datas: [],
+    };
+  },
+  created() {
+    for (let t = 0; t < 10; t++) {
+      this.datas.push("https://picsum.photos/20" + t.toString());
+    }
+  },
+  method: {},
+};
+</script>
